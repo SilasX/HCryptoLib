4277202d81f2ae9e841f561b36f65aee2af15b24
Silas Barta <sbarta@gmail.com>
2013-07-03 20:43:22 -0700

    stripped out identifying string

--- Test.hs
+++ Test.hs
@@ -4,8 +4,8 @@ import qualified To64
 import qualified Table64 as Tab64
 import qualified HexOps as HexO
 
-testHexStr = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-testBase64Str = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+testHexStr = "69b71d79f8218a39259a7a29aabb2dbafc31cb300108310518720928b30d38f411493515597619d76df8e7aefcf74fbf"
+testBase64Str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890+/"
 main = do
     --let b64strInts = Tab64.base64LToInt12 testBase64Str
     --let hexInts = HexO.hexLToInt12 testHexStr
